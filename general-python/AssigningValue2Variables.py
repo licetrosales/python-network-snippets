09cd399564812ceca7432b2150bc586b8c9e77de
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2025-10-29 23:03:42 +0100

    doc: add file assigment of values to variables

---
+++ general-python/AssigningValue2Variables.py
@@ -0,0 +1,16 @@
+# Assigning values to variables
+name = "Luisa"
+age = 12
+score = 72.71
+print (name, "is", age, "years old.") 
+print (name, "scored", score, "percent.")
+
+# Assigning a single value to multiple varaibles  
+x = y= z= 4
+print (x) 
+print (y) 
+print (z)
+
+# Assigning multiple variagles to multiple values
+name, age, score = "Luisa", 12, 72.77
+print (name, "scored", score, "percent.")
