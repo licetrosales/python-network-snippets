77bcdda58294d7cd29349610eddda6324cd7d5dc
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2025-11-12 20:13:27 +0100

    doc: add examples of lists and tuples

---
+++ general-python/ListsAndTuples.py
@@ -0,0 +1,7 @@
+# List and tuples
+list1 = ["January", "Four", 2025, 11.56, "Heaven"]
+tuple1 = ("a", "Sydney", "1900", 3.1416, 0.01, "Pathon")
+tuple2 = "x", "empire", "lego", 1, 2.0, 7
+print (list1)
+print (tuple1)
+print (tuple2)
