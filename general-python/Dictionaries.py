380d90b593feaa4faedf9325d8c8caea2dc9049e
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2025-11-18 16:55:19 +0100

    doc: add Dictionaries.py with example dictionary usage

---
+++ general-python/Dictionaries.py
@@ -0,0 +1,8 @@
+# Creating a dictionary
+myDict = {"ID": 22, "name":"Juan Lozano", "score": 70, "Grade": "C"}
+
+dict1 = dict([(1,"cats"), (2,"radios"), (3, "flowers")])
+class_ranking = dict({1:"Person 1", 2:"Person 2", 3:"Person 3"})
+print (myDict)
+print (dict1)
+print (class_ranking)
