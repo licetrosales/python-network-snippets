f6b347b18a649e761f3055eadbf60689a0195320
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2025-11-23 21:46:16 +0100

    doc: add input and output for a list

--- general-python/IO_Import.py
+++ general-python/IO_Import.py
@@ -2,3 +2,11 @@
 name = input("Enter your name: ")
 age = int(input("enter your age: "))
 print ("Hello,", name + ". You are a ", age, "year old.")
+
+# Save data as a list
+my_cities = input("Name three cities you have lived in: ") 
+city1, city2, city3 = my_cities.split()
+print ("Here are the cities I lived in: \n1. ", city1,  "\n2. ", city2, "\n3. ", city3)
+print(city1)
+print(city2)
+print(city3)
