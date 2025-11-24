659e3e58a3ead66694a681f475889954e0c9333e
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2025-11-24 22:10:56 +0100

    doc: add ImportModules.py to calculate circle area

---
+++ general-python/ImportModules.py
@@ -0,0 +1,6 @@
+# Import the math module
+import math
+r = float(input("Enter the radius of the circle: \n"))
+area = (math.pi*r*r)
+print ( "Radius", r, sep = ": ", end = "cm.\n")
+print ( "Area", area, sep = ": ", end = "sq.cm.\n")
