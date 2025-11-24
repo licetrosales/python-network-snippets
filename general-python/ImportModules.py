534c84da993ea91b8f4132709b64515a60acd283
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2025-11-24 22:11:49 +0100

    doc: import and display Python module search paths
    Added code to import and display Python module search paths.

--- general-python/ImportModules.py
+++ general-python/ImportModules.py
@@ -4,3 +4,8 @@ r = float(input("Enter the radius of the circle: \n"))
 area = (math.pi*r*r)
 print ( "Radius", r, sep = ": ", end = "cm.\n")
 print ( "Area", area, sep = ": ", end = "sq.cm.\n")
+
+# Import modules
+import sys
+for path in sys.path:
+    print(path)         #This will show you all the directories Python is using to search for modules.
