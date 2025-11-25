f8b17c91295cd82b9b4d96f2cc52c6b4d645931b
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2025-11-25 21:40:13 +0100

    doc: implement even-odd check with if-else statement
    Added an if-else statement to check if a number is odd.

--- general-python/DecisionMaking.py
+++ general-python/DecisionMaking.py
@@ -2,3 +2,10 @@
 number = int(input("Enter a nummber to check if it is even: "))
 if number % 2 == 0:
     print (number, "is an even number.")
+
+# if - else statement
+number2 = int(input("Enter a nummber to check if it is even: "))
+if number2 % 2 ==0:
+    print (number2, "is an even number.")
+else:
+     print (number2, "is an odd number.")
