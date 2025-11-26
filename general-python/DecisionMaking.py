335b97e69099b4b913a7b886d862330b372c3423
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2025-11-26 21:49:52 +0100

    doc: add age classification using if-elif-else statements

--- general-python/DecisionMaking.py
+++ general-python/DecisionMaking.py
@@ -9,3 +9,18 @@ if number2 % 2 ==0:
     print (number2, "is an even number.")
 else:
      print (number2, "is an odd number.")
+
+# if - elif - else statement
+age = int(input("How old are you?")) 
+if age < 0:
+    print ("Age cannot be less than 0.")
+elif age < 12:
+    print ("You are a child")
+elif age < 18:
+    print ("You are not an adult")
+elif age < 60:
+    print ("You are an adult")
+elif age < 100:
+    print ("You are an senior citizen")
+else:
+    print ("You have entered an invalid age.")
