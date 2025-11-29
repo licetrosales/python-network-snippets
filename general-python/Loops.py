16374363f7d897e5cb7e28d6d163a834f053aead
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2025-11-29 18:46:46 +0100

    doc: implement while loop for user-defined iterations
    Added a while loop to count iterations based on user input.

--- general-python/Loops.py
+++ general-python/Loops.py
@@ -1,3 +1,14 @@
 # For- Loop
 for x in range (0,6):
     print ("Iteration x =", x)
+
+# While - Loop
+iterations = 0
+x = 1
+xmax = eval(input("Enter the maximum integer for the number of iterations: "))
+while x <= xmax:
+    iterations += 1
+    print (iterations)
+    x +=1
+
+print("the number of iterations is", iterations)
