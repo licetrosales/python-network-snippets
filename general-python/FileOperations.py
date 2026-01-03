fefd6d9204b376436dab601449f1493c6daf76cb
Lis Ullmann <54541600+licetrosales@users.noreply.github.com>
2026-01-03 23:24:51 +0100

    doc: use readline method to read the first line of the file
    Added a method to read a single line from the file after reading the entire content.

--- general-python/FileOperations.py
+++ general-python/FileOperations.py
@@ -1,4 +1,8 @@
 # Open and read the entire contents of 'days.txt' file
 my_text = open("/Coding/Projects py/days.txt")
 print(my_text.read())
+
+# Read the content of a file using the method readline()
+print(my_text.readline())
+
 my_text.close()
